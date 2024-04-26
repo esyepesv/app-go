fc9c08ea12405c93e101fc10b004d1fdf5a89700
esyepesv <60147085+esyepesv@users.noreply.github.com>
2024-04-25 23:20:23 -0500

    coverage gate test

--- coverage.sh
+++
@@ -1,8 +0,0 @@
-COVERAGE_PERCENTAGE=$(go tool cover -func=coverage.out | grep total: | awk '{print $3}' | sed 's/\%//g')
- 
-if [ "$COVERAGE_PERCENTAGE" -lt "70" ]; then
-    echo "Code coverage ($COVERAGE_PERCENTAGE%) is below the required threshold (70%)"
-    exit 1  # Exit with a non-zero status to indicate an error
-else
-    echo "Code coverage ($COVERAGE_PERCENTAGE%) meets the required threshold"
-fi
